600ab8af18f5bcd60cc0dd0a09de342e95eecb5f
ricardoyanezvillagomez <72508079+ricardoyanezvillagomez@users.noreply.github.com>
2020-12-08 09:24:48 -0600

    Add files via upload
    actualizacion categorias

--- JS/estilo-index.js
+++ JS/estilo-index.js
@@ -5,11 +5,71 @@
 */
 
 function mostrarBeverages(){
-    document.getElementById("imgBeverages").style.display="block";
+	document.getElementById("imgBeverages").style.display="block";
+	document.getElementById("imgDairyProducts").style.display="none";
     document.getElementById("imgCondiments").style.display="none";
+    document.getElementById("imgConfections").style.display="none";
+    document.getElementById("imgCereals").style.display="none";
+    document.getElementById("imgMeat").style.display="none";
+    document.getElementById("imgProduce").style.display="none";
 }
 
 function mostrarCondiments(){
     document.getElementById("imgCondiments").style.display="block";
     document.getElementById("imgBeverages").style.display="none";
+    document.getElementById("imgConfections").style.display="none";
+    document.getElementById("imgCDairyProducts").style.display="none";
+    document.getElementById("imgCereals").style.display="none";
+    document.getElementById("imgMeat").style.display="none";
+    document.getElementById("imgProduce").style.display="none";
+}
+function mostrarConfections()
+{
+	document.getElementById("imgConfections").style.display="block";
+	document.getElementById("imgCondiments").style.display="none";
+	document.getElementById("imgBeverages").style.display="none";
+	document.getElementById("imgDairyProducts").style.display="none";
+	document.getElementById("imgCereals").style.display="none";
+	document.getElementById("imgMeat").style.display="none";
+	document.getElementById("imgProduce").style.display="none";
+}
+function mostrarDairyProducts()
+{
+	document.getElementById("imgDairyProducts").style.display="block";
+	document.getElementById("imgConfections").style.display="none";
+	document.getElementById("imgCondiments").style.display="none";
+	document.getElementById("imgBeverages").style.display="none";
+	document.getElementById("imgCereals").style.display="none";
+	document.getElementById("imgMeat").style.display="none";
+	document.getElementById("imgProduce").style.display="none";
+}
+function mostrarCereals()
+{
+	document.getElementById("imgCereals").style.display="block";
+	document.getElementById("imgDairyProducts").style.display="none";
+	document.getElementById("imgConfections").style.display="none";
+	document.getElementById("imgCondiments").style.display="none";
+	document.getElementById("imgBeverages").style.display="none";
+	document.getElementById("imgMeat").style.display="none";
+	document.getElementById("imgProduce").style.display="none";
+}
+function mostrarMeat()
+{
+	document.getElementById("imgMeat").style.display="block";
+	document.getElementById("imgCereals").style.display="none";
+	document.getElementById("imgDairyProducts").style.display="none";
+	document.getElementById("imgConfections").style.display="none";
+	document.getElementById("imgCondiments").style.display="none";
+	document.getElementById("imgBeverages").style.display="none";
+	document.getElementById("imgProduce").style.display="none";
+}
+function mostrarProduce
+{
+	document.getElementById("imgProduce").style.display="block";
+	document.getElementById("imgMeat").style.display="none";
+	document.getElementById("imgCereals").style.display="none";
+	document.getElementById("imgDairyProducts").style.display="none";
+	document.getElementById("imgConfections").style.display="none";
+	document.getElementById("imgCondiments").style.display="none";
+	document.getElementById("imgBeverages").style.display="none";
 }
