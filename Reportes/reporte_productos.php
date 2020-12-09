678f2433db461f79ca129004c7a01518e136160c
SEBASTIAN PEREZ ALVAREZ <183110521702946@cbtis217.edu.mx>
2020-12-09 12:47:02 -0600

    Arreglando problemas con las referencias a los CSS

--- Reportes/reporte_productos.php
+++ Reportes/reporte_productos.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="CSS/reporte_pro.css">
+        <link rel="stylesheet" href="../CSS/reporte_pro.css">
         <title>Report Products</title>
 </head>
     <body>
